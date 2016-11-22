6c2535be7f53f3dc8d896f757d536a3fdeaa3a90
zhaoshicheng <2814432992@qq.com>
2016-11-22 14:04:00 +0800

    create

---
+++ firework.c
@@ -0,0 +1,11 @@
+//  #include <graphics.h>
+// #include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+// #include <Mmsystem.h>
+#pragma comment(lib,"Winmm.lib")
+
+int main(){
+
+}
